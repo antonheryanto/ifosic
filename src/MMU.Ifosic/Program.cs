397c8bd939b9c7bc69f3313cbfe5c0dbdfc27bab
Anton Heryanto Hasan <anton.heryanto@gmail.com>
2023-05-03 19:19:37 +0800

    clean code

--- src/MMU.Ifosic/Program.cs
+++ src/MMU.Ifosic/Program.cs
@@ -12,62 +12,48 @@ var fiberId = 1;
 var name = Path.Combine(root, $"Set01_Time_Parallel.zip");
 //var name = Path.Combine(project, $"{id}.bin");
 var fdd = FrequencyShiftDistance.Load(name);
+//FrequencyShiftDistance.ToMessagePack(img, Path.Combine(srcPath, $"Set0{id}.msgpack"));
+fdd.AddReference(@"C:\Projects\MMU\Set01\Set01_Pressure vs Time.xlsx");
+fdd.Save($@"C:\Projects\MMU\projects\{id}.bin");
+//fdd.ToMessagePack($@"C:\Projects\MMU\ifosic\src\Python\Set0{id}.msgpack");
+
+//Augment(Path.Combine(project, "1.bin"));
 
 //var img = Signal.Resize(fdd.Traces);
 //var ng = new NumberToGrid(-20, 30, 64);
 //var ngC = ng.GetClass(27);
+var (cxt, dxt) = Signal.GetBoundary(fdd, Path.Combine(srcPath, "model_512_64.onnx"));
+//var catD = fdd.Categories
+//		//.Where((s, i) => i > 600 && i < 1250)
+//		.Select(s => (double)s).ToList();
 
-//FrequencyShiftDistance.ToMessagePack(img, Path.Combine(srcPath, $"Set0{id}.msgpack"));
-//fdd.AddReference(@"C:\Projects\MMU\Set01\Set01_Pressure vs Time.xlsx");
-// get the distance
-fdd.Save($@"C:\Projects\MMU\projects\{id}.bin");
-//fdd.ToMessagePack($@"C:\Projects\MMU\ifosic\src\Python\Set0{id}.msgpack");
+static void Plot(params PlotView[] plots)
+{
+	var t = new TableLayoutPanel
+	{
+		Location = new System.Drawing.Point(0, 0),
+		Dock = DockStyle.Fill,
+		RowCount = plots.Length,
+	};
+	var rowSize = 100F / t.RowCount;
+	for (int i = 0; i < plots.Length; i++)
+	{
+		t.RowStyles.Add(new RowStyle(SizeType.Percent, rowSize));
+		t.Controls.Add(plots[i]);
+	}
+	var f = new Form() { Text = "Hello NativeAOT!", Width = 1024, Height = 1024 };
+	f.Controls.Add(t);
+	Application.Run(f);
+}
 
 // parallel fiber layout
 // time based switch, one iteration, multiple iteration
 // time to fiber switch info
 // boundary is distance to fiber switch
 
-//Augment(fdd);
-return;
-
-var (cxt, dxt) = Signal.GetBoundary(fdd, Path.Combine(srcPath, "model_512_64.onnx"));
-var catD = fdd.Categories
-		//.Where((s, i) => i > 600 && i < 1250)
-		.Select(s => (double)s).ToList();
-
-var plots = new PlotView[] {
-    //new PlotView().PlotHeatmap(ng.GetItem(ix[600]), max: 1),
-    //new PlotView().PlotHeatmap(fdd, max: 25),
-    new PlotView()
-		 .PlotLine(dxt)
-		 .PlotBoundary(cxt)
-		 .PlotLine(catD),
-         //.PlotBoundary(fdd.BoundaryIndexes),
-    //new PlotView()
-    //    .PlotLine(conv)
-    //    .PlotLine(convDist)
-    //    .PlotScatter(averagePoint),
-};
-
-var t = new TableLayoutPanel
+static void Augment(string fileName)
 {
-	Location = new System.Drawing.Point(0, 0),
-	Dock = DockStyle.Fill,
-	RowCount = plots.Length,
-};
-var rowSize = 100F / t.RowCount;
-for (int i = 0; i < plots.Length; i++)
-{
-	t.RowStyles.Add(new RowStyle(SizeType.Percent, rowSize));
-	t.Controls.Add(plots[i]);
-}
-var f = new Form() { Text = "Hello NativeAOT!", Width = 1024, Height = 1024 };
-f.Controls.Add(t);
-Application.Run(f);
-
-static void Augment(FrequencyShiftDistance? fdd)
-{
+	var fdd = FrequencyShiftDistance.Load(fileName);
 	if (fdd is null)
 		return;
 	var serialTimeIndex = new List<int>();
@@ -80,11 +66,11 @@ static void Augment(FrequencyShiftDistance? fdd)
 	}
 	var fiberIndex = 2;
 	var boundaries = new List<double>();
-	for (int i = fiberIndex; i < fiberIndex + 3; i++)
+	for (int i = fiberIndex + 1; i < fiberIndex + 4; i++)
 	{
 		boundaries.Add(fdd.Boundaries[i]);
 	}
-	// generate parallel set using serial set 1, fiber 3, 4, 5
+	// generate parallel set using serial set 1, fiber 4, 5
 	// uses boundary as anchor, put fiber one to the selected fiber, append the reminder 
 
 	var serialTraces = new List<double[]>();
